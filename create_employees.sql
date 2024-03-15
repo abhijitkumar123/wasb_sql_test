3e18095101c2cec48ca61619c3fec3b6ed06be80
Tejal Nijai <57373269+Tvn2005@users.noreply.github.com>
2024-03-15 00:22:10 +0000

    Update create_employees.sql
    Created Employee Table as per instructions given data in hr folder.
    Inserted data as mentioned in file.
    To test my logic works right for find_manager_cycles.sql logic as there is no loop exists in the given data,inserted 2 records.

---
+++ create_employees.sql
@@ -0,0 +1,38 @@
+--create_employees.sql
+--Use the schema
+use memory.default;
+
+--Create Employee Table
+create table EMPLOYEE(
+employee_id TINYINT
+,first_name VARCHAR
+,last_name VARCHAR
+,job_title VARCHAR
+,manager_id TINYINT
+);
+
+--Insert Data into Employee Table
+Insert into EMPLOYEE
+Values (1, 'Ian', 'James','CEO',4);
+Insert into EMPLOYEE
+Values (2, 'Umberto', 'Torrielli','CSO',1);
+Insert into EMPLOYEE
+Values (3, 'Alex', 'Jacobson','MD EMEA',2);
+Insert into EMPLOYEE
+Values (4, 'Darren', 'Poynton','CFO',2);
+Insert into EMPLOYEE
+Values (5, 'Tim', 'Beard','MD APAC',2);
+Insert into EMPLOYEE
+Values (6, 'Gemma', 'Dodd','COS',1);
+Insert into EMPLOYEE
+Values (7, 'Lisa', 'Platten','CHR',6);
+Insert into EMPLOYEE
+Values (8, 'Stefano', 'Camisaca','GM Activation',2);
+Insert into EMPLOYEE
+Values (9, 'Andrea', 'Ghibaudi','MD NAM',2);
+
+--Test records inserted by me
+Insert into EMPLOYEE
+Values (10, 'Test1_First', 'Test1_Last','MD1 NAM',11);
+Insert into EMPLOYEE
+Values (11, 'Test2_First', 'Test2_Last','MD2 NAM',10);
